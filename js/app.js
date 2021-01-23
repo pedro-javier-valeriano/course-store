c738538d97907a464f9044b0b22988932cf8de25
pedro javier <pedro.javier.valeriano.99@gmail.com>
2021-01-23 10:17:52 -0500

    ya podemos agregar elementos al carritp

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //variables
 const carrito= document.getElementById('carrito');
 const cursos= document.getElementById('lista-cursos');
-
+const listaCursos= document.querySelector('#lista-carrito tbody');
 //listeners
 cargarEventListeners();
 function cargarEventListeners(){
@@ -19,6 +19,7 @@ function comprarCurso(e){
     //delegaicion para agregar carrito
     if(e.target.classList.contains('agregar-carrito')){
         const curso= e.target.parentElement.parentElement;
+        //enviamos el curso seleccionado para tomar sus datos
         leerDatosCurso(curso);
     }
     
@@ -26,6 +27,28 @@ function comprarCurso(e){
 
 //lee los datos del curso
 function leerDatosCurso(curso){
-    console.log(curso);
+    const infoCurso={
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
 
+    }
+    insertarCarrito(infoCurso);
+}
+
+//muestra el curso seleccionado en el carrito
+function insertarCarrito(curso){
+    const row= document.createElement('tr');
+    row.innerHTML=`
+    <td>
+        <img src="${curso.imagen}"width=100/>
+    <td/>
+    <td>${curso.titulo}</td>
+    <td>${curso.precio}</td>
+    <td>
+        <a href="#" class="borrar-curso" data-id="${curso.id}">x</a>
+    </td>
+    `;
+    listaCursos.appendChild(row);
 }
